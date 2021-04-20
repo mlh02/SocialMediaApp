3c9342b6acbc87801116cbf79967664b1ba44e19
Montather Handal <ali.louy2005@gmail.com>
2021-04-19 19:07:22 -0500

    Added User Model

---
+++ SocialMedia_ApplicationV1/Models/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocialMedia_ApplicationV1.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ SocialMedia_ApplicationV1/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialMedia_ApplicationV1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Image { get; set; }
+
+    }
+}
