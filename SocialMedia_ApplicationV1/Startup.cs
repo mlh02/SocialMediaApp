92ff4e05f579322720a68b3246aeff05ad8c14e6
Montather Handal <ali.louy2005@gmail.com>
2021-04-20 23:17:48 -0500

    added context to app

--- SocialMedia_ApplicationV1/Startup.cs
+++ SocialMedia_ApplicationV1/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SocialMedia_ApplicationV1.Models;
 
 namespace SocialMedia_ApplicationV1
 {
@@ -24,7 +26,9 @@ namespace SocialMedia_ApplicationV1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DataBaseContext>(item => item.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
